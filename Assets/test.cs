674e780ecb223ec3e67831438f3a6f1ec8a0f3e6
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-02-15 23:48:58 +0900

    スクリプトのEditorをRiderに変更して”Hellow,World”

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,17 +6,7 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int[] points = { 5, 7, 3, 6 };
-
-		int sum = 0;
-		for (int i = 0; i < points.Length; i++) {
-			sum += points [i];
-		}
-
-		int average = sum / points.Length;
-
-		Debug.Log (average);
-	
+	    Debug.Log("Hello, World");
 	}
 	
 	// Update is called once per frame
