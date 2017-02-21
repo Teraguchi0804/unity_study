0b67fdeeb14d714c99d092a989674e96261aea79
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-02-22 00:52:12 +0900

    modify

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,7 +6,14 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	    Debug.Log("Hello, World!!");
+//	    Debug.Log("Hello, World!!");
+//	    int age;
+//	    age = 30;
+//	    Debug.Log(age);
+	    float height1 = 160.5f;
+	    float height2;
+	    height2 = height1;
+	    Debug.Log(height2);
 	}
 	
 	// Update is called once per frame
