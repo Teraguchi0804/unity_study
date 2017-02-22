f5cd531f9cc7c5b736ea7241ea26b7460f583362
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-02-23 00:43:59 +0900

    study0223

--- Assets/test.cs
+++ Assets/test.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class test : MonoBehaviour {
 
     // Use this for initialization
-    void Start () {
-//	    Debug.Log("Hello, World!!");
-//	    int age;
-//	    age = 30;
-//	    Debug.Log(age);
-        float height1 = 160.5f;
-        float height2;
-        height2 = height1;
-        Debug.Log(height2);
+    void Start (){
+        int herbNum = 1;
+        if (herbNum == 1){
+            for (int i = 0; i < 5; i++){
+                Debug.Log("HPが50回復");
+            }
+        }
     }
 
     // Update is called once per frame
